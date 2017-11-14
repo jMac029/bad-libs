c21c93e68d46d89c5a0a04d5f0d87087f6a2a305
James McMillan <jmcmillan29@gmail.com>
2017-11-14 12:17:24 -0800

    started the preliminary code to start the back-end, added giphy api and ajax call to the api

--- assets/javascript/controller.js
+++ assets/javascript/controller.js
@@ -1,2 +1,38 @@
 // This is where all the javascript will
- // live for the project that connects the view and model
+// live for the project that connects the view and model
+
+// giphy api info:
+
+
+
+// Controller Object for all of our Controller Functions
+var controller = {
+
+	// variables to use within the controller object
+	apiKey: "oXTcsygddx6gGqHTrnCdyGto2t9XZfy6",
+	queryURL: "https://api.giphy.com/v1/gifs/search?q=" +
+	        searchWord + "&api_key=" + this.apiKey + "&limit=5"
+
+	// function to search Giphy and parse out results
+	searchGiphy: () => {
+
+		$.ajax({
+	        url: contoller.queryURL,
+	        method: "GET"
+	      }).done(function(giphyResponse) {
+
+	      	// declare JSON output into a results variable for easier use
+	      	let giphyResults = giphyResponse.data
+
+	      	// console log the results
+	      	console.log(giphyResults)
+
+	      	// loop through results to parse out what we want to display from the api
+	      	for (var i = 0; i < giphyResults.length; i++) {
+
+	      	}
+
+
+	      });
+	},
+}
--- assets/javascript/model.js
+++ assets/javascript/model.js
@@ -1,2 +1,6 @@
 // This is where all the data and global variables will 
  // live for the project
+
+ // Global Variables
+
+ var searchWord;
