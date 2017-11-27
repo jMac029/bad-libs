14880b7ff703c2ab1fbf774828dcd2f77c46c098
James McMillan <jmcmillan29@gmail.com>
2017-11-26 20:37:49 -0800

    removed all the semi-colons from our code, created the playAgain function and linked it up to dom

--- assets/javascript/model.js
+++ assets/javascript/model.js
@@ -11,20 +11,22 @@
  var badLibIndex = 0;
  var termCounter = 0;
  var allTermsCounter = 0;
+ var sentenceCounter = 0;
  var authorName = "";
  var searchTerms = 0;
 
-//Creating a variable for each sentence in the Mad Lib
- var sentence01 = "A <span id='noun-01'></span> in Kentucky was arrested this morning after he <span id='verb-01'></span> in front of a house." 
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDy-LoCZBFI3byY029kxJbSzM-3CbPquaQ",
+    authDomain: "bad-libs-ab833.firebaseapp.com",
+    databaseURL: "https://bad-libs-ab833.firebaseio.com",
+    projectId: "bad-libs-ab833",
+    storageBucket: "",
+    messagingSenderId: "729741352036"
+  };
+  firebase.initializeApp(config);
 
-
- /*(Proper noun) had a history of (verb), but no one - not even his (noun) - ever imagined he’d (verb) with a (noun) stuck in his (body part). 
- \'I always thought he was (adjective), but i never thought he’d do something like this. Even his (relative) was surprised.'\ 
- After a brief (activity), cops followed him to a (fast food restaurant), where he reportedly (adjective, past tense) in the fry machine. 
- In (month), a woman was charged with a similar crime. 
- But rather than (verb), a with a (noun), she (verb, past tense) with a (adjective) dog. 
- Either way, we imagine that after witnessing him (verb) with a (noun) there are probably a whole lot of (plural noun) that are going to need some therapy.*/
-
+var database = firebase.database();
 
 var badLibsArray = [ 
 
--- assets/javascript/view.js
+++ assets/javascript/view.js
@@ -4,17 +4,8 @@
  // On Page Load
 $(document).ready(function(){
 
-	// input whatever needs to happen when the page loads
-	view.displayWelcomeScreen();
-	//controller.startBadLibButtonClicked();
-	//controller.termButtonClicked();
-	//controller.nextSentenceButtonClicked();
-	//controller.selectRandomBadLib();
-	//controller.loopTermsToSearchForInput();
-	//console.log("badLibsArray: " + badLibsArray);
-	//console.log(badLibs.badLibs1[0].searchedTerms[0].term);
-	//view.displayTermInputs();
-
+	view.displayWelcomeScreen()
+	
 });
 
 // view object for manipulation of the view with JS and jQuery
@@ -31,9 +22,9 @@ var view = {
 	              	"<button class='clear button' id='start-badlib-button'>Begin Your Story</button>" +
 	            "<form>" +
 	        "</div>"
-	    $('input').focus();
-        $('.welcome-screen').append(welcomeScreenHtml);
-        controller.startBadLibButtonClicked();
+	    $('input').focus()
+        $('.welcome-screen').append(welcomeScreenHtml)
+        controller.startBadLibButtonClicked()
 
 	},
 
@@ -56,49 +47,45 @@ var view = {
 	    $('.welcome-screen').hide()
 	    $('.user-term-inputs').empty()
 	    $('.user-term-inputs').append(inputHtml)
-	    $('input').focus();
-	    controller.termButtonClicked();
+	    $('input').focus()
+	    controller.termButtonClicked()
 	    // searchTermCounter++
 	},
 
 	displayBadLibHeader: () => {
 		var badLibTitle = "<h1 id='story-title'>" + chosenBadLib.title + "</h1>"
 		var badLibAuthor = "<h3 id='story-author'> by " + authorName + "</h3>"
-		$('.story-display').prepend(badLibTitle, badLibAuthor);
+		$('.story-display').prepend(badLibTitle, badLibAuthor)
 	},
 
 	displayBadLib: () => {
+		console.log("sentenceCounter: " + sentenceCounter)
 		$('.user-term-inputs').hide()
-		var badLibSentence = randomBadLib[badLibIndex].sentence
+		var badLibSentence = randomBadLib[sentenceCounter].sentence
+		console.log("badLibSentence: " + badLibSentence)
 
-		// var termsToSearch = badLibs.badLibs1[0].termsToSearch
-		var gifs = randomBadLib[badLibIndex].searchedTerms[0].gif
-		var terms = randomBadLib[badLibIndex].searchedTerms[0].term
-		console.log(gifs)
-		// searchTermCounter--
+		var gifs = randomBadLib[sentenceCounter].searchedTerms[0].gif
+		var terms = randomBadLib[sentenceCounter].searchedTerms[0].term
+		console.log("gifs: " + gifs)
+		console.log("terms: " + terms)
 		var nextSentenceButton = "<div class='cell small-6 float-center'><button class='clear button next-sentence-button'>next Sentence in Bad Lib</button></div>"
-		var allDone = "<div class='cell small-6 float-center'><button class='clear button all-done-button'>all done</button></div>"
+		var playGameAgain = "<div class='cell small-6 float-center'><button class='clear button play-again-button'>Play Again?</button></div>"
 		$('.bad-lib-display').append(badLibSentence);
-		if ( badLibIndex < randomBadLib.length ) {
+		if ( sentenceCounter < randomBadLib.length-1 ) {
 			$('.bad-lib-display').append(nextSentenceButton)
+			controller.nextSentenceButtonClicked()
 		} 
-		// still working on getting this to work
-		if ( badLibIndex === randomBadLib.length ) {
-			$('.bad-lib-display').append(allDone)
+		if ( sentenceCounter === randomBadLib.length-1 ) {
+			$('.bad-lib-display').append(playGameAgain)
+			controller.playGameAgainButtonClicked()
 		}
-		//var speechSentence = $('#sentence').text()
-		//console.log(speechSentence)
-		//responsiveVoice.setDefaultVoice("US English Female");
-		//responsiveVoice.speak($('#sentence').text(), "US English Female", {rate: .5});
-		controller.nextSentenceButtonClicked();
-		// controller.textToSpeech();
 		for (var i = 0; i < gifs.length; i++) {
-			var gifImage = $('<img>');
-			gifImage.attr("src", gifs[i].images.fixed_height.url);
-			gifImage.attr("title", terms[i]);
+			var gifImage = $('<img>')
+			gifImage.attr("src", gifs[i].images.fixed_height.url)
+			gifImage.attr("title", terms[i])
 			//gifImage.text(terms[i]);
-			gifImage.prepend(terms[i]);
-			$('#term'+i).html(gifImage);
+			gifImage.prepend(terms[i])
+			$('#term'+i).html(gifImage)
 		} return controller.textToSpeech()
 		
 	}
